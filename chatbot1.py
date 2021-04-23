b8882f7ca005fd0b036ded4f9d57cad9fd2c3bd5
NingJingwen <20440456@life.hkbu.edu.hk>
2021-04-23 23:39:16 +0800

    security

--- chatbot1.py
+++ chatbot1.py
@@ -6,17 +6,19 @@ import configparser
 import logging
 import redis
 
+import os
+
 global redis1
 
 def main():
     # Load your token and create an Updater for your Bot
-    config = configparser.ConfigParser()
-    config.read('config.ini')
-    updater = Updater(token=(config['TELEGRAM']['ACCESS_TOKEN']), use_context=True)
+    # config = configparser.ConfigParser()
+    # config.read('config.ini')
+    updater = Updater(token=(os.environ['ACCESS_TOKEN']), use_context=True)
     dispatcher = updater.dispatcher
     # You can set this logging module, so you will know when and why things do not work as expected
     global redis1
-    redis1 = redis.Redis(host=(config['REDIS']['HOST']), password=(config['REDIS']['PASSWORD']), port=(config['REDIS']['REDISPORT']))
+    redis1 = redis.Redis(host=(os.['HOST']), password=(os.environ['PASSWORD']), port=(os.environ['REDISPORT']))
     
     logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                         level=logging.INFO)
